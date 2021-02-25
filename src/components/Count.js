1a1a668121edda660b2b2e0c0ed57d3e037a8425
jayhspark <jayhspark@gmail.com>
2021-02-25 11:52:15 +0900

    finish toolkit reducer

--- src/components/Count.js
+++ src/components/Count.js
@@ -1,18 +1,30 @@
 import React, {useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {increment, decrement, incrementByAmount} from '../redux/counter'
 const Count = () => {
-  const [count, setCount] = useState(0)
-  const increment = () => {
-    setCount(count + 1)
+  const { count } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
+  console.log(count)
+  // const [count, setCount] = useState(0)
+  const incresement = () => {
+    dispatch(increment())
+    // setCount(count + 1)
   }
-  const decrement = () => {
-    setCount(count - 1) 
+  const decresement = () => {
+    dispatch(decrement())
+    // setCount(count - 1) 
   }
+  const incresementByMount = () => {
+    dispatch(incrementByAmount(33))
+  }
+
   return (  
     <div>
       <h1>this is count</h1>
       <p>the count is {count}</p>
-      <button onClick={increment} style={{height:20}}>increment</button>
-      <button onClick={decrement} style={{height:20}}>decrement</button>
+      <button onClick={incresement} style={{height:20}}>increment</button>
+      <button onClick={decresement} style={{height:20}}>decrement</button>
+      <button onClick={incresementByMount} style={{height:20}}>incrementByAmount</button>
     </div>
   );
 }
--- src/redux/counter.js
+++ src/redux/counter.js
@@ -6,11 +6,11 @@ export const counterSlice = createSlice({
     count:0
   },
   reducers:{
-    increment: state => {
-      state.count +=1
+    increment: (state) => {
+      state.count += 1
     },
-    decrement: state => {
-      state.count-=1
+    decrement: (state) => {
+      state.count -= 1
     },
     incrementByAmount: (state, action) => {
       state.count += action.payload
